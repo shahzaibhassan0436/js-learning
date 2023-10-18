b487c718e5574a9c2c559effa2efb004603869c3
shahzaibhassan0436 <148256596+shahzaibhassan0436@users.noreply.github.com>
2023-10-18 17:28:54 +0000

    container config done

--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,1 +1,19 @@
-console.log("Shahzaib Hassan Diraj")
+const accountId = 48000
+let accountEmail = "shahzaibdiraj480@gmail.com"
+var accountPassword = "12334"
+accountCity = "Lahore"
+let accountState;
+
+
+accountEmail = "zohaibhassan1576@gmail.com"
+accountPassword = "11 ghagh"
+accountCity = "Shorkot"
+
+
+/*
+Prefer not to use var
+Because of issue in block in scope and functional scope
+*/
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
