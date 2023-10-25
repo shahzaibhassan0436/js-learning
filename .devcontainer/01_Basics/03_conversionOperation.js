6746b104a717cd95912d106d51cd3ad33670f000
shahzaibhassan0436 <148256596+shahzaibhassan0436@users.noreply.github.com>
2023-10-25 10:24:46 +0000

    conversion of datatypes

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Shahzaib"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn) 
+
+// 1 => true , 0 => false
+// "" => false , "Value"=> true
+
+let someNumber = 480
+let stringNumber =  String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
--- .devcontainer/01_Basics/03_conversionOperaton.js
+++
@@ -1,7 +0,0 @@
-let score = "33abc"
-
-console.log(typeof score);
-console.log(typeof(score));
-
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
