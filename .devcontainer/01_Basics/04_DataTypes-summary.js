2be78179599ef1917bff5a79c12d7aa38d5e6b6b
shahzaibhassan0436 <148256596+shahzaibhassan0436@users.noreply.github.com>
2023-10-26 18:21:15 +0000

    dataTypes

---
+++ .devcontainer/01_Basics/04_DataTypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+// 7 Types : String ,Number, Boolean ,Null, Undefined , Symbol, BigInt
+
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail ; 
+
+const id = Symbol('480')
+const anotherId = Symbol('480')
+
+console.log(id === anotherId);
+
+const bigNumber = 432746926263823704n
+
+
+// Refrence Type/Non-Premitive 
+// : Arrays, Objects, Functions
+
+const heros = ["Muhhamad ALi Jinnah" , "Allam Iqbal" , "Sir Syed"];
+let myObj = {
+    name : "Shahzaib",
+    age : 27 ,
+}
+
+const myFunction = function(){
+    console.log("Shahzaib Hassan");
+}
+
+
+console.log(typeof anotherId);
